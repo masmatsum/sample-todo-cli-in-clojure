cc8041ad1a6f3fd00cf2393595e9fe1dccc77a7b
masmatsum <masmatsum@users.noreply.github.com>
2021-04-22 00:03:11 +0900

    Commit files generated by `lein new app`

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sample-todo-cli-in-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/sample_todo_cli_in_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns sample-todo-cli-in-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [sample-todo-cli-in-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
