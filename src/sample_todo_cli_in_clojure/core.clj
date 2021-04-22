05455b3036e7eaaf4a621b85837106336c101652
masmatsum <masmatsum@users.noreply.github.com>
2021-04-22 22:31:03 +0900

    [WIP] Process cmd args

--- src/sample_todo_cli_in_clojure/core.clj
+++ src/sample_todo_cli_in_clojure/core.clj
@@ -1,7 +1,41 @@
 (ns sample-todo-cli-in-clojure.core
   (:gen-class))
 
+(def usage
+  "Usage:
+  - add {task-name}
+  - list
+  - finish {item-id}
+  - unfinish {item-id}")
+
+(defn add-todo
+  [[task-name]]
+  ;; TODO:
+  (println (str "Added task \"" task-name "\".")))
+
+;; TODO:
+(defn list-todo
+  [arg-list])
+
+;; TODO:
+(defn finish-todo
+  [arg-list])
+
+;; TODO:
+(defn unfinish-todo
+  [arg-list])
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [[subcommand & arg-list] args]
+    (case subcommand
+      "add" (add-todo arg-list)
+      "list" (list-todo arg-list)
+      "finish" (finish-todo arg-list)
+      "unfinish" (unfinish-todo arg-list)
+      nil (do
+            (println "Please specify subcommand.")
+            (println usage))
+      (do
+        (println (str "Invalid subcommand: " subcommand))
+        (println usage)))))
